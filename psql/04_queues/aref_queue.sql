178bcf2b642608ee909f6457417074aa8f75f181
jawerg <janwergula@gmail.com>
2019-10-05 12:30:49 +0200

    deleted waste

--- psql/04_queues/aref_queue.sql
+++ psql/04_queues/aref_queue.sql
@@ -2,10 +2,3 @@ create view PK.VIEW_QUEUE_AREF as
 select
     AREF
     from PK.IDX_AID;
-
-
-select *
-    from INFORMATION_SCHEMA.TABLES
-    where TABLE_SCHEMA = 'pg_catalog';
-
-select pg_relation_filepath('pk.ct_journal_rank');
