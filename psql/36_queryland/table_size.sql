92686f6d317ccb0185c13827332038366f3cb333
jawerg <janwergula@gmail.com>
2019-10-06 22:09:37 +0200

    reasonable refurbishment of internet-based copy-paste stuff...

--- psql/36_queryland/table_size.sql
+++ psql/36_queryland/table_size.sql
@@ -1,7 +1,14 @@
+with
+    TABLES as (
+        select TABLE_SCHEMA || '.' || TABLE_NAME as TABNAME
+            from INFORMATION_SCHEMA.TABLES
+            where TABLE_SCHEMA = 'pk'
+              and TABLE_TYPE = 'BASE TABLE' -- views do not have a reasonable size...
+    )
 select
-    TABLE_SCHEMA || '.' || TABLE_NAME                                                         as TABLE_FULL_NAME,
-    pg_size_pretty(pg_total_relation_size('"' || TABLE_SCHEMA || '"."' || TABLE_NAME || '"')) as SIZE
-    from INFORMATION_SCHEMA.TABLES
-    where TABLE_SCHEMA = 'pk'
-      and pg_total_relation_size('"' || TABLE_SCHEMA || '"."' || TABLE_NAME || '"') > 0
-    order by pg_total_relation_size('"' || TABLE_SCHEMA || '"."' || TABLE_NAME || '"') desc;
+    TABNAME,
+    pg_size_pretty(pg_table_size(TABNAME))          as TABLE_SIZE,
+    pg_size_pretty(pg_indexes_size(TABNAME))        as INDEX_SIZE,
+    pg_size_pretty(pg_total_relation_size(TABNAME)) as TOTAL_SIZE
+    from TABLES
+    order by pg_total_relation_size(TABNAME) desc;
